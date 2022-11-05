8c67c256fc420ba14031a19ba75b62be327e76c1
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-11-05 19:51:21 +0100

    Replace name of terraform-bucket

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
    backend "s3" {
-    bucket = "terraform-state-12312d123f"
+    bucket = "terraform-ip-bucket"
     key    = "terraform.tfstate"
     region = "us-west-2"
   }
