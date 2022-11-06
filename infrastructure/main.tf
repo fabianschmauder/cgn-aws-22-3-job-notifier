d8289c65b4b22a8fcb2d8d04f522acf714dd1c58
Justice Chima Onuoha <45771491+Jay2cof@users.noreply.github.com>
2022-11-06 22:36:36 +0100

    Update main.tf
    scrip added to terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,3 +17,12 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {}
+
+#!/bin/bash
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+cd /home/ec2-user
+aws s3 cp s3://lambdabucket101/api-server.zip api-server.zip 
+unzip api-server.zip
+pip3 install -r requirements.txt
+cd src
+python3 main.py
