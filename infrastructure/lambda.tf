b4a3b39664a6f1fb26fb246c6d38c564063e04ea
Martin Koeller <koeller2203@gmail.com>
2022-11-06 21:58:16 +0100

    update infrastructure

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,10 +1,10 @@
 resource "aws_lambda_function" "test_lambda" {
-  function_name = "jobvault"
+  function_name = "jobvault_lambda"
   filename = "lambda.zip"
-  role     = "arn:aws:iam::874515606678:role/LabRole"
+  role     = "arn:aws:iam::326202573469:role/LabRole"
   runtime  = "python3.9"
   layers   = [aws_lambda_layer_version.lambda_layer.arn]
-  handler  = "index.handler"
+  handler  = "jobvault_lambda.lambda_handler"
 }
 
 resource "aws_lambda_layer_version" "lambda_layer" {
