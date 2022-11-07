53cf4c2474b651f7ac1b687cc15e41aaf1e676b9
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-11-07 02:28:42 +0100

    Add execution of job-bucket-script and zipping  of the lambda-layer

--- upload-src.sh
+++ upload-src.sh
@@ -1,5 +1,6 @@
 # without sandbox please move creation of the bucket to terraform
 sh setup_src_bucket.sh
+sh setup_job_bucket.sh
 
 mkdir build
 
@@ -8,6 +9,14 @@ cd api-server
 zip ../build/api-server.zip requirements.txt
 zip -r ../build/api-server.zip src 
 cd ..
+
+echo "requests-layer for job-lambda"
+mkdir python
+cd python
+pip3 install requests -t .
+cd ..
+zip -r ../build/requests_layer.zip python
+rm -rf python
 
 echo  "upload to s3"
 aws s3 cp build/api-server.zip s3://ip-src-bucket/
