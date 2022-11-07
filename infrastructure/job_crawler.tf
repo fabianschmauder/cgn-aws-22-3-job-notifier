1aa040c5956144ed6e1b7bf269d7043733eee2e6
Fabian Schmauder <fabian.schmauder@gmail.com>
2022-11-07 13:15:46 +0100

    Fix add instances to target group

--- infrastructure/job_crawler.tf
+++ infrastructure/job_crawler.tf
@@ -18,3 +18,23 @@ resource "aws_lambda_layer_version" "requests_layer" {
 
   compatible_runtimes = ["python3.9"]
 }
+
+resource "aws_cloudwatch_event_rule" "every_five_minutes" {
+    name = "every-five-minutes"
+    description = "Fires every five minutes"
+    schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "job_crawler_every_five_minutes" {
+    rule = aws_cloudwatch_event_rule.every_five_minutes.name
+    target_id = "crawl-jobs"
+    arn = aws_lambda_function.job_crawler.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_job_crawler" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.job_crawler.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
+}
